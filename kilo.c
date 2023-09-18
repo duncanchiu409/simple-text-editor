a835891af9f2a215577c69e60f162ff486e61def
duncanchiu409 <chiuduncan409@gmail.com>
2023-09-18 13:37:56 +0930

    [add] turn off echoing

--- kilo.c
+++ kilo.c
@@ -1,7 +1,20 @@
 #include<stdio.h>
 #include<unistd.h>
+#include<termios.h>
+
+void enable_raw_mode(){
+  struct termios raw;
+
+  tcgetattr(STDIN_FILENO, &raw);
+
+  raw.c_cflag &= ~(ECHO); // bitwise AND operation on raw.c_cflag and ~(ECHO) and put inside raw.c_cflag
+  
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main(int argc, char* argv[]){
+  enable_raw_mode();
+  
   printf("Hello World:\n");
 
   char c;
